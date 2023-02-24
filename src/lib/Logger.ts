59e6f439412b178a682e168650795ecf322ece67
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-23 23:11:45 -0300

    update logger

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -1,7 +1,7 @@
 import debug from "debug";
 
 export class Logger {
-    public static debug = debug("botd:debug");
+    public static debug = debug("bot:debug");
     public static info = debug("bot:info");
     public static warn = debug("bot:warn");
     public static error = debug("bot:error");
