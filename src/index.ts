d4fe930f03a7e3483474ff80ea2b93e370d1c31a
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-24 23:39:09 -0300

    add index structure and dependency singleton

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import "dotenv/config";
+import { DependencySingleton } from "./services/DependencySingleton";
+import { Client } from "./lib/Client";
+
 async function main() {
-    console.log("Hello World");
+    const client = Client.createDefault<DependencySingleton>(process.env.DISCORD_TOKEN ?? "");
+
+    client.useDependencySingleton(DependencySingleton.getInstance());
+
+    client.autowireCommands("./commands");
+
+    client.autowireEvents("./events");
+
+    client.login();
 }
 main();
