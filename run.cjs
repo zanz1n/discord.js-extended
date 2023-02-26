fe0b87b262431b36c526874356c76fa138ae2b51
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-26 02:22:59 -0300

    update custom ts dev compiler

--- run.cjs
+++ run.cjs
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+"use strict";
 function reduce(arr, n) {
     for (let i = 0; i < n; i++) {
         arr.pop();
@@ -5,15 +7,20 @@ function reduce(arr, n) {
 }
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { spawnSync } = require("child_process");
-const exit = process.exit;
-process.exit = (c) => {
-    console.log(`Ts compilation exited with code ${c}`);
-    if (c != 0) exit(c);
-};
-reduce(process.argv, 4);
-require("./node_modules/.bin/tsc");
-process.exit = exit;
-let result;
-if (process.argv.includes("--worker")) result = spawnSync("node", ["./dist/worker.js"], { stdio: "inherit" });
-else result = spawnSync("node", ["./dist/index.js"], { stdio: "inherit" });
-process.exit(result.status ?? 0);
+
+function run() {
+    spawnSync("rm", ["-r", "./dist"], { stdio: "inherit" });
+    const exit = process.exit;
+    process.exit = (c) => {
+        console.log(`TS compilation exited with code ${c}`);
+        if (c != 0) exit(c);
+    };
+    reduce(process.argv, 4);
+    require("./node_modules/.bin/tsc");
+    process.exit = exit;
+    let result;
+    if (process.argv.includes("--worker")) result = spawnSync("node", ["./dist/worker.js"], { stdio: "inherit" });
+    else result = spawnSync("node", ["./dist/index.js"], { stdio: "inherit" });
+    process.exit(result.status ?? 0);
+}
+run();
