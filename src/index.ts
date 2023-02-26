b0940303cfd62cdf4692d26d7c29489c76297e6f
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-26 10:48:30 -0300

    add export in index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from "discord.js";
+export { Client as BaseClient } from "discord.js";
+export { Client, ListenerCallback } from "./Client.js";
+export * from "./Logger.js";
+export * from "./types/CommandNamespace.js";
+export * from "./types/EventNamespace.js";
+export * from "./types/InvalidNamespaceError.js";
+export * from "./types/SlashCommandInteraction.js";
+export * from "./helpers/mention.js";
+export * from "./helpers/object-includes-key.js";
