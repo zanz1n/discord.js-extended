4b18aff93e53e70e8cbd6155bfd7b822a82f6df3
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-26 14:16:04 -0300

    update publish package

--- scripts/publish.cjs
+++ scripts/publish.cjs
@@ -2,5 +2,28 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const { spawnSync } = require("child_process");
-spawnSync("node", ["scripts/build.cjs"], { stdio: "inherit" });
-spawnSync("npm", ["--scope=@zanz1n", "--access", "public"]);
+const packageJson = require("../package.json");
+const { writeFileSync } = require("fs");
+const { join } = require("path");
+
+let version = process.argv.find((arg) => arg.startsWith("--version="))?.replace("--version=", "");
+
+if (!version) {
+    console.error("No version specified");
+    process.exit(1);
+}
+
+const buildProcess = spawnSync("node", ["scripts/build.cjs"], { stdio: "inherit" });
+if (buildProcess.status !== 0) {
+    console.log("Build failed");
+    process.exit(buildProcess.status);
+}
+
+packageJson.version = version;
+writeFileSync(join(__dirname, "..", "package.json"), JSON.stringify(packageJson, null, 2) + "\n");
+
+const publishProcess = spawnSync("npm", ["publish", "--scope=@zanz1n", "--access", "public"], { stdio: "inherit" });
+if (publishProcess.status !== 0) {
+    console.log("Publish failed");
+    process.exit(buildProcess.status);
+}
