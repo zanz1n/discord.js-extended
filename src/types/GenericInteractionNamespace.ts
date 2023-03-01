39d818ec04c24266196581b8bd00846153f4f314
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-02-28 21:18:02 -0300

    implement generic interactions

---
+++ src/types/GenericInteractionNamespace.ts
@@ -0,0 +1,32 @@
+import { Client } from "../Client.js";
+import { 
+    ApplicationCommandData,
+    ButtonInteraction,
+    ChatInputApplicationCommandData,
+    ChatInputCommandInteraction,
+    Interaction
+} from "discord.js";
+
+export enum GenericInteractionType {
+    ButtonIntegrable = "BUTTON_INTEGRABLE"
+}
+
+export type ButtonIntegrableInteractio = ApplicationCommandData | ChatInputCommandInteraction | ButtonInteraction
+
+export interface GenericInteractionProps {
+    enabled: boolean
+    type: GenericInteractionType
+}
+
+export interface GenericInteractionRunOpts<T extends Interaction, Singleton> {
+    client: Client<Singleton>
+    interaction: T
+}
+
+export type GenericInteractionData = ChatInputApplicationCommandData
+
+export interface GenericInteractionNamespace {
+    genericInteractionProps: GenericInteractionProps
+    genericInteractionData: GenericInteractionData
+    handle(opts: GenericInteractionRunOpts<Interaction, any>): Promise<void>   
+}
